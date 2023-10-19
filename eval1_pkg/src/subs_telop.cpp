74dd61c712c598b2a9534b14f5d9f4ff35ba88b8
rpit21 <rubennevaldo@gmail.com>
2023-10-19 17:01:55 -0500

    Leds

--- eval1_pkg/src/subs_telop.cpp
+++ eval1_pkg/src/subs_telop.cpp
@@ -21,6 +21,12 @@ int stop;
 #define IN1 3 // change pin number here
 #define IN2 4 // change pin number here
 
+#define LED1 0
+#define LED2 5
+#define LED3 6
+#define LED4 13
+#define LED5 19
+
 void callback(const geometry_msgs::Twist::ConstPtr & msg)
 {
 
@@ -29,7 +35,7 @@ void callback(const geometry_msgs::Twist::ConstPtr & msg)
 	vel=10*vel;
 	pwm=pwm+vel;
 
-	if (stop==0){
+	if (stop==1){
 		pwm=0;
 
 	} else {
@@ -42,7 +48,49 @@ void callback(const geometry_msgs::Twist::ConstPtr & msg)
 		}
 	}
 
+	if (pwm=20){
+		digitalWrite(LED1, HIGH);
+		digitalWrite(LED2, LOW);
+		digitalWrite(LED3, LOW);
+		digitalWrite(LED4, LOW);
+		digitalWrite(LED5, LOW);
 
+	}else if (pwm=40){
+		digitalWrite(LED1, HIGH);
+		digitalWrite(LED2, HIGH);
+		digitalWrite(LED3, LOW);
+		digitalWrite(LED4, LOW);
+		digitalWrite(LED5, LOW);
+
+	}else if (pwm=60){
+		digitalWrite(LED1, HIGH);
+		digitalWrite(LED2, HIGH);
+		digitalWrite(LED3, HIGH);
+		digitalWrite(LED4, LOW);
+		digitalWrite(LED5, LOW);
+
+
+	}else if (pwm=80){
+		digitalWrite(LED1, HIGH);
+		digitalWrite(LED2, HIGH);
+		digitalWrite(LED3, HIGH);
+		digitalWrite(LED4, HIGH);
+		digitalWrite(LED5, LOW);
+
+
+	}else if (pwm>=100){
+		digitalWrite(LED1, HIGH);
+		digitalWrite(LED2, HIGH);
+		digitalWrite(LED3, HIGH);
+		digitalWrite(LED4, HIGH);
+		digitalWrite(LED5, HIGH);
+	}else{
+		digitalWrite(LED1, LOW);
+		digitalWrite(LED2, LOW);
+		digitalWrite(LED3, LOW);
+		digitalWrite(LED4, LOW);
+		digitalWrite(LED5, LOW);
+	}
 
 	softPwmWrite(pin,pwm);
 
@@ -56,17 +104,19 @@ void callback_sentido (const std_msgs::Int32::ConstPtr & msg){
 	if (sentido==1){
 		digitalWrite(IN1, HIGH);
 		digitalWrite(IN2, LOW);
+		std::cout<<"Sentido:Horario"<<"\n";
 	}else{
 		digitalWrite(IN1, LOW);
 		digitalWrite(IN2, HIGH);
+		std::cout<<"Sentido:Antihorario"<<"\n";
 	}
 
 }
 
 void callback_parada (const std_msgs::Int32::ConstPtr & msg){
 	stop=msg->data;
-
 }
+
 
 int main(int argc, char **argv)
 
@@ -84,6 +134,7 @@ int main(int argc, char **argv)
 
 	ros::Subscriber parada_subs = node_obj.subscribe("/pul",10,callback_parada);
 
+
 	//configuracion de pines como salida
 
 	wiringPiSetupGpio();
@@ -92,6 +143,12 @@ int main(int argc, char **argv)
 	pinMode(pin,OUTPUT);
 	pinMode(IN1, OUTPUT);
 	pinMode(IN2, OUTPUT);
+
+	pinMode(LED1, OUTPUT);
+	pinMode(LED2, OUTPUT);
+	pinMode(LED3, OUTPUT);
+	pinMode(LED4, OUTPUT);
+	pinMode(LED5, OUTPUT);
 
 	// inicializamos al pin como pwm con un rango
 	softPwmCreate(pin,0,range);
